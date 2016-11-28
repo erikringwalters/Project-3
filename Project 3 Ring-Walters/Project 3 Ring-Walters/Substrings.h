75eb37b5b7c8243bc75d20db3ba97337a5576d37
Erik <erik.ring.walters@gmail.com>
2016-11-27 21:19:15 -0800

    functions are declared in header

--- Project 3 Ring-Walters/Project 3 Ring-Walters/Substrings.h
+++ Project 3 Ring-Walters/Project 3 Ring-Walters/Substrings.h
@@ -1,6 +1,12 @@
 #pragma once
+#include <iostream>
+#include <string>
 class Substrings
 {public:
 	Substrings();
-
+	void noX();
+	void countHi2();
+	void allStar();
+	void countAbc();
+	void strCount();
 };
